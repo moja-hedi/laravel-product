120837967fdae48b856610326df6d8683f3230fc
Srsm Huseiny <huseiny.aram@gmail.com>
2022-12-18 17:03:38 +0330

    initial commit

---
+++ src/Providers/ProductServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Apadana\Product\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
+
+class ProductServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->offerPublishing();
+    }
+
+    public function register()
+    {
+
+    }
+
+    protected function offerPublishing()
+    {
+        if (! function_exists('config_path')) {
+            // function not available and 'publish' not relevant in Lumen
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/product.php' => config_path('product.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_product_tables.php.stub' => $this->getMigrationFileName('create_product_tables.php'),
+        ], 'migrations');
+    }
+
+    protected function registerCommands()
+    {
+        $this->commands([]);
+    }
+
+        /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @return string
+     */
+    protected function getMigrationFileName($migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path.'*_'.$migrationFileName);
+            })
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
+    }
+}
